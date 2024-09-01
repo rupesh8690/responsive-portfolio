5e6c35b53dcb1d22978cfe12854c49401d39cab4
rupesh8690 <rupeshlove8690@gmail.com>
2024-09-01 15:09:02 +0530

    my portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This will bind to 0.0.0.0 instead of localhost
+    port: 3000, // Specify the port you want to use (optional)
+  },
 })
